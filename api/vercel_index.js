df7e2f33238fd475ff9f9366a18e47158bd1968f
woshengqilenie <165399641+woshengqilenie@users.noreply.github.com>
2025-09-13 16:06:04 +0800

    Update vercel_index.js

--- api/vercel_index.js
+++ api/vercel_index.js
@@ -1,9 +1,1 @@
-import { handleRequest } from "../src/handle_request.js";
-
-export const config = {
-  runtime: 'edge' //告诉 Vercel 这是 Edge Function
-};
-
-export default async function handler(req) {
-  return handleRequest(req);
-}
+export { default, config } from '../src/index.js';
